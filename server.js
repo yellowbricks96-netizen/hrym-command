1dd3af3722ba27a648aa3e5118d8f18158b7406f
yellowbricks96-netizen <yellowbricks96@gmail.com>
2026-05-10 18:38:09 -0600

    Update server.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`HRYM Command Center running on port ${PORT}`);
 });
